9c9aa31f6749ccd7d5e2d6d7903db6d457a6575a
Alejandro Jara <alejandroejara@hotmail.com>
2024-05-13 12:25:36 -0300

    Primer commit

---
+++ EjercicioClase2Modulo2/Clientes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase2Modulo2
+{
+    internal class Clientes
+    {
+        public int CodCliente { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public bool Vip { get; set; }
+
+    }
+}
---
+++ EjercicioClase2Modulo2/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace EjercicioClase2Modulo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Utilizando LINQ resolver las siguientes consignas:
+
+            //Carga de datos
+            var lstClientes = new List<Clientes>()
+            {
+                new Clientes() { Apellido = "Jara", Nombre = "Alejandro" ,CodCliente = 123 , Vip = true},
+                new Clientes() { Apellido = "Mossier", Nombre = "Fernando" ,CodCliente = 345 , Vip = false},
+                new Clientes() { Apellido = "Bustos", Nombre = "Andres" ,CodCliente = 567 , Vip = true},
+                new Clientes() { Apellido = "Dalpiaz", Nombre = "Carla" ,CodCliente = 789 , Vip = true},
+                new Clientes() { Apellido = "Miranda", Nombre = "Micaela" ,CodCliente = 112 , Vip = false},
+                new Clientes() { Apellido = "De Castillo", Nombre = "Andrea" ,CodCliente = 223 , Vip = false},
+            };
+
+
+            #region Ejercicio1
+
+            // Detectar cual es el numero mas grande e imprimirlo por consola
+
+            var lstNumeros = new List<int>() { 25,10,99,105, 1, 84, 22};
+
+            #endregion
+
+            #region Ejercicio2
+
+            //Ordenar los nombres alfabeticamente
+            var lstNombres = new List<string>() { "Ana", "Alejandro", "Alexis", "Pablo", "Carlos", "Anibal", "Carla", "Susana" };
+
+
+            #endregion
+
+            #region Ejercicio3
+            // Utilizando la variable "lstClientes" filtrar los clientes que tengan vip = true e imprimirlo por consola
+
+            #endregion
+
+            #region Ejercicio4 
+
+            //Utilizando la variable "lstClientes" crear una nueva lista donde solo se encuentren los nombres de los clientes e imprimir los nombres
+
+            #endregion
+
+            #region Ejercicio5
+            //Apartir de la variable "lstClientes" crear una lista que contenga todos los datos pero  modificando los siguientes campos:
+            // Nombre tiene que guardarse en mayusculas
+            // Apellido tiene que guardarse en mayusculas
+            // Vip => se debe evaluar el bool y si es true se debe asignar el texto "Premium" y si es false "Normal"
+            #endregion
+        }
+    }
+}
